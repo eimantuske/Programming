0cb37bd3440ec05f9a48d0774585dcc8bfb8e736
Eimantas <eimantuske59@gmail.com>
2026-04-23 20:47:21 +0300

    sporto klubas viekia

--- Kontrolinis_darbas/SportoKlubas.cpp
+++ Kontrolinis_darbas/SportoKlubas.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 class Narys {
     private:
+
         string vardas;
         string pavarde;
         int metai;
@@ -41,14 +42,76 @@ class Narys {
         };
 
         virtual ~Narys() {}
+    };
+
+class VipNarys : public Narys {
+    private:
+        const double VipNuolaida = 0.35;
+    public:
+        VipNarys(string vardas, string pavarde, int metai, double MetinisMokestis) : Narys(vardas, pavarde, metai, MetinisMokestis) {
+            this -> VipNuolaida;
+        };
+        virtual double SkaiciuotiMokesti() override {
+            return getMetinisMokestis() * (1 - VipNuolaida);
+        };
+    };
+
+Narys* sukurtiNari() {
+    string vardas, pavarde;
+    int metai;
+    int VipStatus;
+
+    cout << "Įveskite nario vardą: ";
+    cin >> vardas;
+    cout << "Įveskite nario pavardę: ";
+    cin >> pavarde;
+    cout << "Įveskite nario amžių: ";
+    cin >> metai;
+    cout << "Įveskite nario Vip statusą (1 - Vip, 0 - ne Vip): ";
+    cin >> VipStatus;
+    double mokestis = 1260.0; 
+    try {
+        if (VipStatus == 1) {
+            return new VipNarys(vardas, pavarde, metai, mokestis);
+        } else {
+            return new Narys(vardas, pavarde, metai, mokestis);
+        }
+    } catch (const invalid_argument& e) {
+        cerr << "Klaida: " << e.what() << endl;
+        return nullptr;
+    }
 };
 
-int main() {
-    Narys* narys1 = new Narys("Jonas", "Jonaitis", 30, 1000);
-    VIPnarys* vipNarys1 = new VIPnarys("Petras", "Petraitis", 70, 1500, 0.45, 0.25);
-    
-    cout << narys1->getVardas() << " metinis mokestis: " << narys1->SkaiciuotiMokesti() << " EUR" << endl;
-    cout << vipNarys1->getVardas() << " metinis mokestis: " << vipNarys1->SkaiciuotiMokesti() << " EUR" << endl;
+int main() { 
+
+    char pasirinkimas;
+
+    cout << "\nSveiki atvykę į sporto klubą!\n" << endl;
+    cout << "Ar norite registruotis kaip narys? (y/n): ";
+    cin >> pasirinkimas;
+
+    switch (pasirinkimas) {
+        case 'y':
+        {
+            cout <<"\n"<<endl;
+            cout << "--- Nario registracija ---" << endl;
+            Narys* naujasNarys = sukurtiNari();
+
+            if (naujasNarys != nullptr) {
+                cout << "\nRegistracija sekminga!" << endl;
+                cout << naujasNarys->getVardas() << " metinis mokestis: " << naujasNarys->SkaiciuotiMokesti() << " EUR\n" << endl;  
+            
+                delete naujasNarys;
+            }
+        }
+            break;
+        case 'n':
+            cout << "Ačiū, kad apsilankėte mūsų sporto klube. Iki pasimatymo!" << endl;
+            return 0;
+        default:
+            cout << "Neteisingas pasirinkimas. Prašome įvesti 'y' arba 'n'." << endl;
+            return 0;
+    }
 
     return 0;
-};
+}
